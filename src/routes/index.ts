2fdd7a92382d1d8ad9d1f2d67781254e79fd4d4d
JoseJR2202 <65959467+JoseJR2202@users.noreply.github.com>
2021-07-12 10:25:01 -0400

    estructura_configuracion_basica

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import session from './session';
+import users from './users';
+
+const router = Router();
+
+router.use('/session', session);
+router.use('/users', users);
+export default router;
